4dc5cc937b06c2c15e314182e90639f31849bc08
Shubhojeet Banerjee <50337427+Shubhojeetban@users.noreply.github.com>
2021-12-15 00:19:21 +0530

    Initial Commit

---
+++ lottery/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'bright ivory cross absent idle start decorate river bleak baby fault sword',
+  'https://rinkeby.infura.io/v3/c4d3dfa23bb840599c9c95a9d9e5ac11'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(interface);
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
